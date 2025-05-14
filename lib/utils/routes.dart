8dbd93ce05e6abac0c8587ef34a8aee64205a7cc
CarlosArroyo21 <carlosdanielarroyocardona11@gmail.com>
2025-05-14 01:39:54 -0500

    routes added

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,5 +1,11 @@
+import 'package:coffee_app/view/home_view.dart';
 import 'package:coffee_app/view/login_view.dart';
+import 'package:coffee_app/view/modify_coffee_view.dart';
+import 'package:coffee_app/view/register_coffee_view.dart';
 
 final kRoutes = {
-  '/login': (context) => const LoginView(),
+  '/login': (context) => LoginView(),
+  '/home': (context) => const HomeView(),
+  '/newCoffee': (context) => const RegisterCoffeeView(),
+  '/editCoffee': (context) => const ModifyCoffeeView(),
 };
