13d5af1910ad8e04c07ca0ef6297e99d8f7e6b4a
CarlosArroyo21 <carlosdanielarroyocardona11@gmail.com>
2025-05-14 01:39:38 -0500

    App palette colors added

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -2,4 +2,6 @@ import 'dart:ui';
 
 const kBackgroundColor = Color(0xFFF2F1F0);
 const kButtonColor = Color(0xFF73482F);
+const kSecondaryColor = Color(0xFF73412F);
 const kTextColor = Color(0xFF262626);
+const kAppBarColor = Color(0xFFBFA799);
